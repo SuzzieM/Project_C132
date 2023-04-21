95f5e557240dbbccb63b29c2fd92bab837a22c1f
SuzzieM <100200764+SuzzieM@users.noreply.github.com>
2023-04-21 13:30:18 +0530

    Add files via upload

---
+++ pet.js
@@ -0,0 +1,39 @@
+img = " ";
+status = " ";
+function setup()
+{
+    canvas = createCanvas(640, 420);
+    canvas.center();
+
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting objects";
+
+}
+
+function modelLoaded()
+{
+    console.log("Model is Loaded and Intialized!!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+function preload()
+{
+   img = loadImage("Pets.jpg");
+}
+
+function draw()
+{
+    image (img, 0 , 0 , 640, 420);
+}
+function gotResult(error, results)
+{
+   if(error)
+   {
+    console.log(error);
+   }
+   else
+   {
+    console.log(results);
+    objects = results;
+   }
+}
